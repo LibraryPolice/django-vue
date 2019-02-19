a118f4f2a2e384b8dac3d46baed659d85e87bbcc
pai <doublepi@outlook.com>
2019-02-19 22:15:02 +0800

    删除未ignore的文件

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,6 +3,5 @@ import myapp.views as views
 
 urlpatterns = [
 url(r'init_book$', views.init_book, ),
-url(r'show_books$', views.show_books, ),
 url(r'upload_file$', views.upload_file, ),
 ]
--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\PAI\PycharmProjects\DVue\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
